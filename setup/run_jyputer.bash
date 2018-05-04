cd81c57aed7f172eb814d57ad34e236b98defcfc
rxl194 <Rendong_Lin@yahoo.com>
2018-05-04 09:44:07 -0700

    .

--- setup/run_jyputer.bash
+++ setup/run_jyputer.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm /tmp/jnotebook.log; 
-/home/rendong/anaconda3/envs/dl4cv/bin/jupyter notebook --ip='*' --port=80 --no-browser --allow-root >& /tmp/jnotebook.log & 
+jupyter notebook --ip='*'  >& /tmp/jnotebook.log & 
