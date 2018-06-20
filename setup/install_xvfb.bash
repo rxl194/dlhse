5075df1d975465fae44e48535f50c6a422f4e926
rxl194 <Rendong_Lin@yahoo.com>
2018-06-20 14:34:37 +0000

    .

---
+++ setup/install_xvfb.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# virtual frame buffer
+# https://github.com/processing/processing/wiki/Running-without-a-Display
+
+
+sudo apt-get install xvfb libxrender1 libxtst6 libxi6 
+
+java -version
+#sudo apt-get install default-jdk
+# or
+#sudo apt-get install default-jre
+
+sudo Xvfb :1 -screen 0 1024x768x24 </dev/null &
+export DISPLAY=":1"
